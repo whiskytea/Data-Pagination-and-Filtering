153f5a5a599ee5d8f7d7fea10551ca490bb2a81a
Admiin <silntmind@gmail.com>
2021-06-23 07:22:26 -0700

    Update script.js
    PAGINATION WORKS and is dynamic. If you reduce the number of objects in the data array in data.js the code still displays correctly.

--- js/script.js
+++ js/script.js
@@ -6,33 +6,29 @@ FSJS Project 2 - Data Pagination and Filtering
 const studentList = document.querySelector('.student-list');
 const linkList = document.querySelector('.link-list');
 
+let totalDataLength = data.length;
+let numberOfPages = Math.ceil(totalDataLength/9);
+
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-/*
-   load of the page its going create all the student divs but only display the first 9
-   js-class for page 1, page 2, page 3 (array length / 9 rounded up)
-   loop through the array, creating the element and adding the js-class property   
-      in chunks of 9
-*/
-
 //function -> creates the StudentItem HTML element
-let createStudentItem = (studentObject) => {
+let createStudentItem = (studentObject, jsClass) => {
    //create all the elements
    let studentItem = document.createElement('LI');
    studentItem.classList.add("student-item");
    studentItem.classList.add("cf");
+   studentItem.classList.add(jsClass);
 
    let studentDetails = document.createElement('DIV');
    studentDetails.className = "student-details";
 
    let avatar = document.createElement('IMG');
    avatar.className = "avatar";
-   
-   // setAltribute function: https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
-   avatar.setAttribute('alt', 'Profile Picture');
+      
+   avatar.setAttribute('alt', 'Profile Picture'); // setAltribute function: https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
    avatar.setAttribute('src', studentObject.picture.large);
 
    let name = document.createElement('H3');
@@ -62,41 +58,74 @@ let createStudentItem = (studentObject) => {
 
 }
 
-let appendItem = () => {
-   for (let item of data){
-      let newDiv = createStudentItem(item);
-      studentList.appendChild(newDiv);
+// function that takes the data array, passes it through the createStudentItem() function
+let createStudentArray = (data, numberOfPages) => {
+   let studentArray = [];
+   let index = 0;
+   for (let i = 1; i <= numberOfPages; i++){ // loops only as many times as there as pagination buttons
+      for (let a = 0; a < 9; a++){ //loops through the array 9 times, as we want to assign classes to the array items in chunks of 9 or less
+         if (index >= data.length){ //break out of the loop when the index reaches final index of array, this allows for last page being less than 9 options
+            break;
+         }
+         let student = createStudentItem(data[index], `js-${i}`)
+         studentArray.push(student);
+         index++;
+      }
    }
+   return studentArray
 };
+
+let studentArray = createStudentArray(data, numberOfPages);
+
+let appendStudent = (studentArray, studentList) => {
+   let activeButton = document.querySelector('button.active');
+   for (let student of studentArray){
+      if (student.classList.contains(`js-${activeButton.textContent}`)){ //emagerhd learning how to check if an item is in a classList was a nightmare: https://blog.kevinchisholm.com/object-oriented-javascript/getting-to-know-the-javascript-element-classlist-object/
+         studentList.appendChild(student);
+      }
+   }
+}
+
+let loadStudents = (studentArray, studentList) => {
+   //clean slate
+   while (studentList.firstChild){
+      studentList.removeChild(studentList.firstChild);
+   }
+
+  //load new list of students
+  appendStudent(studentArray, studentList);
+}
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-/*
-   create a button for each 9 div "page"
-   when a button is pushed, it removes all existing children in the student-list div
-      and then appends all divs with a matching js-class
-*/
-
-// find out how many pagination buttons are needed
-let dataLength = data.length;
-console.log(dataLength);
-let numberOfPages = Math.ceil(dataLength/9);
-console.log(numberOfPages);
-
-//create buttons
-for (let i = 1; i <= numberOfPages; i++){
-   let buttonLI = document.createElement('LI');
-   let button = document.createElement('BUTTON');
-   button.setAttribute('type', 'button');
-   if (i === 1){
-      button.className = 'active';
+let loadPages = () => {
+   //create buttons
+   for (let i = 1; i <= numberOfPages; i++){
+      let buttonLI = document.createElement('LI');
+      let button = document.createElement('BUTTON');
+      button.setAttribute('type', 'button');
+      if (i === 1){
+         button.className = 'active';
+      }
+      button.textContent = i;
+      buttonLI.appendChild(button);
+      linkList.appendChild(buttonLI);
    }
-   button.textContent = i;
-   buttonLI.appendChild(button);
-   linkList.appendChild(buttonLI);
+
+   //collect all the buttons
+   let buttons = document.querySelectorAll('BUTTON');
+   
+   //add the event listenter that will load a new list of students when clicked
+   for (let button of buttons){
+      button.addEventListener('click', () =>{
+         buttons.forEach((button) => {button.classList = ''});
+         button.className = 'active';
+         loadStudents(studentArray, studentList );
+      })
+   }
 }
 
 // Call functions
@@ -107,4 +136,9 @@ for (let i = 1; i <= numberOfPages; i++){
    create all buttons & after all divs created -> append first 9 to the page
 */
 
-appendItem();
+document.addEventListener('DOMContentLoaded', () => {
+   loadPages();
+   appendStudent(studentArray, studentList);
+});
+
+
