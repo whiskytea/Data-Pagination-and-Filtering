3c93f412b2a6cc71afcee3f0a7b5087fbd87276a
Admiin <silntmind@gmail.com>
2021-06-24 17:28:39 -0700

    Final Update
    MAJOR UPDATES:
    - redid the entire pagination process to be dynamic -> before the HTML elements were hard coded all at once -> the new code creates the HTML elements each time a button is clicked
    
    - added search functionality -> searches as you type & gives error message if no results are found

--- js/script.js
+++ js/script.js
@@ -3,24 +3,42 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+//snag the containers for the students and the pagination buttons
 const studentList = document.querySelector('.student-list');
 const linkList = document.querySelector('.link-list');
+const header = document.querySelector('.header');
 
-let totalDataLength = data.length;
-let numberOfPages = Math.ceil(totalDataLength/9);
+//determine number of students per page, and the number of pages needed
+let itemsPerPage = 9;
+let numberOfPages = (array) => {
+   return Math.ceil(array.length/itemsPerPage);
+}
+
+//pay attention to when the search bar is focused
+//if a value is entered
+// -> if a valid entry
+// --> remove the pagination buttons that exist -> start loading pages based on entry
+// 
+
+
+
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
-//function -> creates the StudentItem HTML element
-let createStudentItem = (studentObject, jsClass) => {
+let blankSlate = (container) => {
+   while (container.firstChild){
+      container.removeChild(container.firstChild);
+   }
+}
+
+let createStudentItem = (studentObject) => {
    //create all the elements
    let studentItem = document.createElement('LI');
    studentItem.classList.add("student-item");
    studentItem.classList.add("cf");
-   studentItem.classList.add(jsClass);
 
    let studentDetails = document.createElement('DIV');
    studentDetails.className = "student-details";
@@ -58,52 +76,32 @@ let createStudentItem = (studentObject, jsClass) => {
 
 }
 
-// function that takes the data array, passes it through the createStudentItem() function
-let createStudentArray = (data, numberOfPages) => {
-   let studentArray = [];
-   let index = 0;
-   for (let i = 1; i <= numberOfPages; i++){ // loops only as many times as there as pagination buttons
-      for (let a = 0; a < 9; a++){ //loops through the array 9 times, as we want to assign classes to the array items in chunks of 9 or less
-         if (index >= data.length){ //break out of the loop when the index reaches final index of array, this allows for last page being less than 9 options
-            break;
-         }
-         let student = createStudentItem(data[index], `js-${i}`)
-         studentArray.push(student);
-         index++;
-      }
-   }
-   return studentArray
-};
-
-let studentArray = createStudentArray(data, numberOfPages);
-
-let appendStudent = (studentArray, studentList) => {
-   let activeButton = document.querySelector('button.active');
-   for (let student of studentArray){
-      if (student.classList.contains(`js-${activeButton.textContent}`)){ //emagerhd learning how to check if an item is in a classList was a nightmare: https://blog.kevinchisholm.com/object-oriented-javascript/getting-to-know-the-javascript-element-classlist-object/
-         studentList.appendChild(student);
-      }
+//loops through the data array and only creates HTML elements for the relevant student indexes
+let appendStudent = (pageNumber, data) => {
+   for (let i = itemsPerPage*(pageNumber-1); i < itemsPerPage*(pageNumber) && i < data.length; i++){
+      let student = createStudentItem(data[i]);
+      studentList.appendChild(student); //append student to the studentList
    }
 }
 
-let loadStudents = (studentArray, studentList) => {
-   //clean slate
-   while (studentList.firstChild){
-      studentList.removeChild(studentList.firstChild);
-   }
 
-  //load new list of students
-  appendStudent(studentArray, studentList);
+
+
+let showPage = (pageNumber, data) => {
+   blankSlate(studentList);
+   appendStudent(pageNumber, data);
 }
 
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
 */
 
-let loadPages = () => {
+let addPagination = (array) => {
+   blankSlate(linkList);
+
+   let pageNumber = numberOfPages(array);
    //create buttons
-   for (let i = 1; i <= numberOfPages; i++){
+   for (let i = 1; i <= pageNumber; i++){
       let buttonLI = document.createElement('LI');
       let button = document.createElement('BUTTON');
       button.setAttribute('type', 'button');
@@ -113,32 +111,52 @@ let loadPages = () => {
       button.textContent = i;
       buttonLI.appendChild(button);
       linkList.appendChild(buttonLI);
-   }
-
-   //collect all the buttons
-   let buttons = document.querySelectorAll('BUTTON');
-   
-   //add the event listenter that will load a new list of students when clicked
-   for (let button of buttons){
       button.addEventListener('click', () =>{
+         let buttons = document.querySelectorAll('.link-list button');
+         console.log(buttons);
          buttons.forEach((button) => {button.classList = ''});
          button.className = 'active';
-         loadStudents(studentArray, studentList );
+         showPage(parseInt(button.textContent), array);
       })
    }
 }
 
-// Call functions
+// EXTRA CREDIT
+header.insertAdjacentHTML('beforeend', 
+   `<label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" placeholder="Search by name...">
+   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`
+);
+
+const search = document.querySelector('input');
+search.addEventListener('input', (e) =>{ //https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event
+   let studentArray = [];
+   for (let student of data){
+      let studentName = (student.name.first + ' ' + student.name.last).toLowerCase()
+      if (studentName.includes(e.target.value)){
+         studentArray.push(student)
+      }
+   }
+
+   if (studentArray.length === 0){ //if no results found, display error message
+      blankSlate(studentList);
+      blankSlate(linkList);
+      studentList.innerHTML = `<p>No Results Found. Please clear your entry and try again.</p>`;
+   }else{ //otherwise show the search results
+      addPagination(studentArray);
+      showPage(1, studentArray);
+   }
+});
 
 /*
    DOMContentLoaded event trigger
-   create all divs
-   create all buttons & after all divs created -> append first 9 to the page
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-   loadPages();
-   appendStudent(studentArray, studentList);
+   addPagination(data);
+   showPage(1, data); //loads first pages results every time the site is loaded
 });
 
 
